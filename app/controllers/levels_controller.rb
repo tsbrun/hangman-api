e72fa589d3a4ba0686ffa9e0a6f94da842dd452b
TSephanya Brun <tsbrun@icloud.com>
2022-04-27 19:42:41 -0400

    Render levels as json

---
+++ app/controllers/levels_controller.rb
@@ -0,0 +1,30 @@
+class LevelsController < ApplicationController
+
+  def index 
+    @levels = Level.all 
+    if @levels 
+      render json: {
+        levels: @levels 
+      }
+    else
+      render json: {
+        status: 500, 
+        errors: ['levels not found']
+      }
+    end
+  end
+
+  def show 
+    @level = Level.find(params[:id])
+    if @level 
+      render json: {
+        render: @level 
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['level not found']
+      }
+    end
+  end
+end
---
+++ test/controllers/levels_controller_test.rb
@@ -0,0 +1,4 @@
+require "test_helper"
+
+class LevelsControllerTest < ActionDispatch::IntegrationTest
+end
